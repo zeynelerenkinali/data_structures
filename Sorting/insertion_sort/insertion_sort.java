8a16c7b51fc02f3c587ee79fe18349dde7e8361d
zeynelerenkinali <zeynel.eren.kinali55@erzurum.edu.tr>
2024-11-04 12:12:03 +0300

    insretion_sort on process

--- Sorting/insertion_sort/insertion_sort.java
+++ Sorting/insertion_sort/insertion_sort.java
@@ -1,10 +1,11 @@
 public class insertion_sort 
 {
     /* Algorithm
-     * 1. Initialize Arrays at function
-     * 2. Create a for loop
-     * 3. 
-     * 
+     * 1. Initialize; sorted_array, key
+     * 2. Check the size of the array; appropriate or not
+     * 3. Else; create a for loop.
+     * 4. In order to sort; take the second element of the array as key
+     * 5. 
      */
     public static void main(String[] args) 
     {
@@ -16,7 +17,12 @@ public class insertion_sort
         int array_length = not_sorted_array.length, key;
         int sorted_array[] = new int[array_length];
         // 2.
-        for(int i = 0; i < array_length; i++) 
+        if(array_length < 2)
+        {
+            System.err.println("Not able to sort array : Size is not appropriate.");
+            return -1;
+        }
+        for(int i = 1; i < array_length; i++) 
         {
             key = not_sorted_array[i];
             
