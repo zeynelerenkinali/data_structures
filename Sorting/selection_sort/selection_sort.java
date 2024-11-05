5995b036d41ac8330d96a2d244c43771d432bd70
zeynelerenkinali <zeynel.eren.kinali55@erzurum.edu.tr>
2024-11-05 14:45:50 +0300

    to_be_continue

---
+++ Sorting/selection_sort/selection_sort.java
@@ -0,0 +1,19 @@
+/* 
+ * ---
+ * Logic: Inside the array, selection sort will scan through all the array and select smallest element inside the array,
+ * then, it will swap the selected element with element that started the search with.
+ * ---
+ * Algorithm:
+ * 1. Initialize; array, searchStartIndex, smallestElement.
+ * 2. Start 'for loop' with, length of the array.
+ * 3. 
+ */
+
+
+public class selection_sort 
+{
+    public static void main(String[] args)
+    {
+         
+    }
+}
